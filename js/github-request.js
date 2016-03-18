c629c4476d13e68d83b67e9d14df3adef2cd7baf
Nathan Otto <nathan@Nathans-MacBook-Pro.local>
2016-03-18 16:56:33 -0700

    add more output logic

--- js/github-request.js
+++ js/github-request.js
@@ -2,10 +2,18 @@ var apiKey = require('./../.env').apiKey;
 
 exports.apiRequest = function(userName){
   var user = userName;
+  $('#repo-list').empty();
   $.get('https://api.github.com/users/' + user + '/repos' + '?access_token=' + apiKey).then(function(response){
     console.log(response);
     for(var i = 0; i < response.length; i++){
-      $('#repo-list').append('<li>' + response[i].name + '</li>');
+      var repoName = response[i].name;
+      var description = "";
+      if(response[i].description === ""){
+        description = "No description available"
+      } else {
+        description = response[i].description;
+      };
+      $('#repo-list').append('<h4>' + repoName + '</h4><p>Description: ' + description + '</p>');
     }
   }).fail(function(error){
     console.log(error.responseJSON.message);
